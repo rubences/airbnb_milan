5ae6e9fd2937ecb7e810348a4ff2f1299e3d5f6e
rubences <91731294+rubences@users.noreply.github.com>
2023-10-27 13:29:08 +0000

    cambios

--- airbnb_milan.py
+++ airbnb_milan.py
@@ -7,16 +7,18 @@ import seaborn as sns
 
 df = pd.read_csv("airbnb_Milan.csv", sep=";")
 df.head()
+print(df.shape)
 
 #2. Se pide crear un nuevo dataframe que contenga únicamente las siguientes columnas:     “host_is_superhost”, “host_identity_verified", “bathrooms”, “bedrooms”, “daily_price”, “security deposit”, “minimum_nights”, “number_of_reviews”, “review_scores_rating”
 
 df2 = df[["host_is_superhost", "host_identity_verified", "bathrooms", "bedrooms", "daily_price", "security_deposit", "minimum_nights", "number_of_reviews", "review_scores_rating"]]
 df2.head()
+print(df2.shape)
 
 #3. Se pide cambiar los factores de la variable “host_is_superhost” de 0, 1 a: “SI” y, "NO”.(investigar la función recode).
 
-
-
+df["host_is_superhost"] = df["host_is_superhost"].replace({0: "NO", 1: "SI"})
+print(df["host_is_superhost"].head())
 
 
 #4. Se pide cambiar los factores de la variable “host_identity_verified" de 0, 1 a: “VERIFICA” y "NO VERIFICA”.
@@ -24,24 +26,62 @@ df2.head()
 df2["host_is_superhost"] = df2["host_is_superhost"].replace({0: "NO", 1: "SI"})
 df2["host_identity_verified"] = df2["host_identity_verified"].replace({0: "NO VERIFICA", 1: "VERIFICA"})
 df2.head()
+print(df2.shape)
+
+#5. Se pide cambiar los factores de la variable “security_deposit” de 0, 1 a: “NO” y, "SI”.
+
+df2["security_deposit"] = df2["security_deposit"].replace({0: "NO", 1: "SI"})
+df2.head()
+print(df2.shape)
+
+#6. Se pide cambiar los factores de la variable “review_scores_rating” de 0, 1 a: “NO” y, "SI”.
+df2["review_scores_rating"] = df2["review_scores_rating"].replace({0: "NO", 1: "SI"})
+df2.head()
+print(df2.shape)
+
+#7.Mostrar un resumen estadístico de los datos
+print(df2.describe())
+
+#8.Filtrar el dataset por apartamentos cuyo mínimo de noches sea igual o menor que siete.
+df_filtered = df2[df2["minimum_nights"] <= 7]
+print(df_filtered.shape)
+
+#11. ¿Cuál es el precio medio por día de una habitación en función de si el anfitrión tiene verificado o no su perfil?
+df2.groupby("host_identity_verified")["daily_price"].mean()
+
+#10. Quien tiene un mayor numero de reseñas un super host o un super no host
+df2.groupby("host_is_superhost")["number_of_reviews"].sum()
+
+#11. Sobre la estadística anterior ¿quién tiene la puntuación media más alta?
+df2.groupby("host_is_superhost")["review_scores_rating"].mean()
 
 
+#12 Crea un vector categórico llamado “CATEGORÍA”, en función de que, si para la puntuación de las reseñas tiene de 0 a 49, sea "NO ACONSEJABLE"; de 50 a 75 sea “ESTÁNDAR”; y de 76 a 100 sea “TOP”.
+df2["CATEGORÍA"] = pd.cut(df2["review_scores_rating"], bins=[0, 49, 75, 100], labels=["NO ACONSEJABLE", "ESTÁNDAR", "TOP"])
 
+#13 Mostrar las frecuencias de la variable categoría
+df2["CATEGORÍA"].value_counts()
 
+#14 Obtener el histograma del precio por día
+df2["daily_price"].hist()
 
+#15 Obtener el histograma del precio por día, pero con 20 intervalos
+df2["daily_price"].hist(bins=20)
 
-1.4 Mostrar un resumen estadístico de los datos
+#16 Obtener el histograma del precio por día, pero con 20 intervalos y en color verde
+df2["daily_price"].hist(bins=20, color="green")
 
-1.5 Filtrar el dataset por apartamentos cuyo mínimo de noches sea igual o menor que siete.
+#17 Obtener el histograma del precio por día, pero con 20 intervalos, en color verde y con un título
+df2["daily_price"].hist(bins=20, color="green")
+plt.title("Precio por día")
+plt.xlabel("Precio")
+plt.ylabel("Frecuencia")
+plt.show()
 
-1.6 ¿Cuál es el precio medio por día de una habitación en función de si el anfitrión tiene verificado o no su perfil?
-
-1.7 Quien tiene un mayor numero de reseñas un super host o un super no host
-
-1.8 Sobre la estadística anterior ¿quién tiene la puntuación media más alta?
-
-1.9Crea un vector categórico llamado “CATEGORÍA”, en función de que, si para la puntuación de las reseñas tiene de 0 a 49, sea "NO ACONSEJABLE"; de 50 a 75 sea “ESTÁNDAR”; y de 76 a 100 sea “TOP”.
-
-1.10 Mostrar las frecuencias de la variable categoría
-
-1.11 Obtener el histograma del precio por día
+#18 Obtener el histograma del precio por día, pero con 20 intervalos, en color verde, con un título y con una línea vertical en la media
+df2["daily_price"].hist(bins=20, color="green")
+plt.title("Precio por día")
+plt.xlabel("Precio")
+plt.ylabel("Frecuencia")
+plt.axvline(df2["daily_price"].mean(), color="red")
+plt.show()
